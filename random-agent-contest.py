d911e80adec2ca5c8d54fcf40af0fd10acc3c6d3
solejar <solejar236@gmail.com>
2018-04-16 15:59:03 -0400

    got the first working examples

---
+++ random-agent-contest.py
@@ -0,0 +1,15 @@
+from retro_contest.local import make
+
+def main():
+    env = make(game='SonicTheHedgehog-Genesis',state='LabyrinthZone.Act1')
+    obs = env.reset()
+    while True:
+        action = env.action_space.sample()
+        action[7] = 1 #always walk right
+        obs, rew, done, info = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
+
+if __name__ == '__main__':
+    main()
